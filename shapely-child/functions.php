39d62b788fb0280115f9e0956edaca396002fd36
juliedi <julie.di@codeur.online>
2018-02-19 15:13:29 +0100

    first commit

---
+++ shapely-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Shapely functions and definitions.
+ *
+ * @link    https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Shapely
+ */
+
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+		
